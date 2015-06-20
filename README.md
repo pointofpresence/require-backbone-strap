5133bbc0976d86a9e27900b30b8654933fec194a
pointofpresence <git@pointofpresence.ru>
2015-06-20 15:12:33 +0600

    1.0.8 Migrated to resampled-date

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# Require Backbone Strap v1.0.8
+# Require Backbone Strap v1.0.9
 ## aka require-backbone-strap
 
 Author: pointofpresence
 
-Last build date: Sat, 20 Jun 2015 08:20:15 GMT
+Last build date: Sat, 20 Jun 2015 09:12:02 GMT
 
 Boilerplate for RequireJS/Backbone/Bootstrap/Gulp based projects.
--- gulpfile.js
+++ gulpfile.js
@@ -17,6 +17,7 @@ fs           = require("fs"),                 // fs
 autoprefixer = require("gulp-autoprefixer"),  // CSS autoprefixer
 gutil        = require("gulp-util"),          // log and other
 chalk        = require("chalk"),              // colors
+dateHelper   = require("resampled-date"),     // date helper
 replace      = require("gulp-replace");       // replace
 
 var src     = "./src",
@@ -46,36 +47,6 @@ var banner = [
     ' */',
     ''
 ].join('\n');
-
-var dateHelper = {
-    twoDigits: function (d) {
-        if (0 <= d && d < 10)
-            return "0" + d.toString();
-
-        if (-10 < d && d < 0)
-            return "-0" + (-1 * d).toString();
-
-        return d.toString();
-    },
-
-    /**
-     * @param {Date} d
-     */
-    toMysqlFormat: function (d) {
-        return d.getUTCFullYear() + "-" + this.twoDigits(1 + d.getUTCMonth())
-            + "-" + this.twoDigits(d.getUTCDate()) + " "
-            + this.twoDigits(d.getUTCHours())
-            + ":" + this.twoDigits(d.getUTCMinutes()) + ":"
-            + this.twoDigits(d.getUTCSeconds());
-    },
-
-    /**
-     * @param {Date} d
-     */
-    toUnixTimestamp: function (d) {
-        return Math.floor(d.getTime() / 1000);
-    }
-};
 
 function buildAmd() {
     gulp
